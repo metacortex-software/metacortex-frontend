0e54056ff30aa55d4496809cca928beecd826c26
mvchn <movchan@gmail.com>
2023-03-14 22:33:21 +0200

    fix symbols

--- js/matrix.js
+++ js/matrix.js
@@ -17,7 +17,7 @@ function matrix () {
     ctx.font = '14pt monospace';
 
     ypos.forEach((y, ind) => {
-        const text = String.fromCharCode(Math.random() * 128);
+        const text = String.fromCharCode(3e4+Math.random()*33);
         const x = ind * 20;
         ctx.fillText(text, x, y);
         if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
