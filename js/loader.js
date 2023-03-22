0cc61a1c30c048a121731d113d4b59c27b62d5dc
Romаn Mоvchan <movchan@gmail.com>
2023-03-22 17:51:16 +0200

    update html and add js script loader (#10)
    * update html and add js script loader
    
    * update index html
    
    * fix path
    
    ---------
    
    Co-authored-by: Arthur Kravchenko <arturkra92@gmail.com>

---
+++ js/loader.js
@@ -0,0 +1,27 @@
+const text = document.querySelector('._text'); 
+const stringText = text.textContent; 
+const splitText = stringText.split(''); 
+text.textContent = ''; 
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += '<span>' + splitText[i] + '</span>'; 
+}
+let letter = 0; 
+let timer = setInterval(onTick, 100);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[letter];
+    span.classList.add('enterX');
+    letter++
+    if (letter === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer); 
+    timer = null;
+    setTimeout(function() {
+        text.innerHTML = '<a href="/mtrx.html"><button class="loader_button">Enter The Matrix</button></a>';
+    }, 2000);
+}
